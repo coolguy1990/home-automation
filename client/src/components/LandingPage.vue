f3da526fa17911ff04ab7b8bb4c4416d1fa725ad
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2017-05-27 11:54:09 +0530

    added basic material ui template for dashboard page

---
+++ client/src/components/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Landing Page for Home Automation</p>
+
+    <router-link :to="{ name: 'home'}">Dashboard</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LandingPage',
+  }
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,17 @@
-import Hello from '@/components/Hello.vue'
+import Dashboard from '@/components/Dashboard.vue'
+import LandingPage from '@/components/LandingPage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'landing.page',
-    component: Hello
-  }
+    component: LandingPage,
+  },
+  {
+    path: '/dashboard',
+    name: 'home',
+    component: Dashboard,
+  },
 ]
 
 export default routes
